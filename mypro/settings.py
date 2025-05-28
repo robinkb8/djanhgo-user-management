ecc75dcdc34cee72da7e4c11f64ef2ef3b143e5f
roibnkb8 <robinkb888@gmail.com>
2025-05-28 17:43:09 +0530

    add render domain to ALLOWED HOSTS

--- mypro/settings.py
+++ mypro/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-user-management.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
