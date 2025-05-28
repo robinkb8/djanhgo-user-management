028c567a486943a6557fe7b0f6fe41a853e02331
roibnkb8 <robinkb888@gmail.com>
2025-05-28 17:49:03 +0530

    django name for project was djanhgo

--- mypro/settings.py
+++ mypro/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 # Database
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'djanhgo.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
