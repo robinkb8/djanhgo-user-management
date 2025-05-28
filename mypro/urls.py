c3c597c76fa70b86fed5496ef84e641881b2f352
roibnkb8 <robinkb888@gmail.com>
2025-05-28 17:38:38 +0530

    error solved

--- mypro/urls.py
+++ mypro/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.dashboard_view, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('auth/google/', views.google_auth, name='google_auth'),
+    path('admin/', admin.site.urls),
+    path('', include('usermanage.urls')),
 ]
